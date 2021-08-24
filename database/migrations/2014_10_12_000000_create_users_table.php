3dad0f3ce603282c3f1367dadbc1bd1621ba78b7
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-24 17:03:39 +0300

    added users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -15,21 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique('users_name_unique');
-            $table->string('email')->unique('users_email_unique');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
             $table->string('duty')->nullable();
-            $table->boolean('status')->default(0);
+            $table->boolean('status')->default('0');
             $table->string('avatar')->nullable();
-            $table->tinyInteger('verified')->nullable();
+            $table->boolean('verified')->nullable();
             $table->boolean('p_submitted')->default(0);
             $table->boolean('t_submitted')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+           ;
             $table->timestamps();
-            $table->string('messenger_color')->default('#2180f3');
-            $table->boolean('dark_mode')->default(0);
-            $table->boolean('active_status')->default(0);
         });
     }
 
